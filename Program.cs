1dc5ab3b7214e0f9103dd260833cc46e3efdfa9a
IRMuhammadjon <mirahmatullayev@gmail.com>
2023-10-16 12:10:39 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Diagnostics.Metrics;
+using CanculaTOR;
+
+
+bool loop = true;
+while (loop)
+{    
+    Console.WriteLine("1. Add two numbers");
+    Console.WriteLine("2. Add list of numbers");
+    string choose = Console.ReadLine();
+    if (choose == "1")
+    {
+        Console.WriteLine("Enter first number:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Result:{funcions.Add_two_numbers(a, b)}");
+    }
+    else if (choose == "2")
+    {
+        Console.WriteLine("Enter comma - separated(,) integer values:");
+        string text = Console.ReadLine();
+        Console.WriteLine($"Result:{funcions.Add_list_of_numbers(text)}");
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("we don't have such caint of category ! If you want to try again press Enter: ");
+        
+        if (Console.ReadKey().Key != ConsoleKey.Enter)
+        {
+            loop = false;
+        }
+    }
+}
---
+++ funcions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanculaTOR
+{
+    static class funcions
+    {
+        public static int Add_two_numbers(int a,int b)
+        {
+            return a + b;
+        }
+
+        public static int Add_list_of_numbers(string text)
+        {
+            int[] ia = text.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+            return ia.Sum();
+        }
+    }
+
+}
